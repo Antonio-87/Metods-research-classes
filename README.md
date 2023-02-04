03234052fadd2352947e2daddcc1657d7794ca7a
Antonio-87 <davydenko.87@list.ru>
2023-02-04 20:47:09 +0300

    correct _export

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Классы и наследованиею Методы.
+# Классы и наследование. Методы.
 
 [![Build status](https://ci.appveyor.com/api/projects/status/a1mahtd0qrkgkohb?svg=true)](https://ci.appveyor.com/project/Antonio-87/metods-research-classes)
--- src/js/character.js
+++ src/js/character.js
@@ -1,4 +1,4 @@
-export default class Character {
+class Character {
     constructor(name, type) {
         if ((typeof (name) === 'string' && typeof (type) === 'string')
             && (name.length >= 2 && name.length <= 10)
@@ -40,3 +40,5 @@ damage(points) {
         this.health = 0;
     }
 }
+
+export {Character as default, levelUp, demage};
