5ed37a6074438145a2d066ae370c05b302112214
Antonio-87 <davydenko.87@list.ru>
2023-02-05 22:34:46 +0300

    revision_2

--- src/js/character.js
+++ src/js/character.js
@@ -8,13 +8,19 @@ export default class Character {
             this.type = type;
             this.health = 100;
             this.level = 1;
-        } else if (name instanceof TypeError) {
+        } else if (typeof (name) !== 'string') {
             throw new Error(
                 `
-                name - строка, min - 2 символа, max - 10\n
+                name - строка
                 `,
             );
-        } else if (type instanceof TypeError) {
+        } else if (name.length < 2 || name.length > 10) {
+            throw new Error(
+                `
+                name: min - 2 символа, max - 10
+                `,
+            );
+        } else if (typeof (type) !== 'string') {
             throw new Error(
                 `
                 type - один из типов (строка): Bowman, Swordsman, Magician, Daemon, Undead, Zombie
